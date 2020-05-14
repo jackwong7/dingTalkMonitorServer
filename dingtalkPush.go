53db4f0a9f28a04a284da1f906156c2ba869b19f
JackWong <wangchangat@163.com>
2020-05-14 22:57:34 +0800

    修改cpu检测方式

--- dingtalkPush.go
+++ dingtalkPush.go
@@ -6,7 +6,6 @@ import (
 	"encoding/base64"
 	"flag"
 	"fmt"
-	"github.com/shirou/gopsutil/cpu"
 	"github.com/shirou/gopsutil/mem"
 	"gostudy/basic/req/handle"
 	"io/ioutil"
@@ -94,15 +93,52 @@ func getRandomString(l int) string {
 	}
 	return string(result)
 }
+func getCPUSample() (idle, total uint64) {
+	contents, err := ioutil.ReadFile("/proc/stat")
+	if err != nil {
+		return
+	}
+	lines := strings.Split(string(contents), "\n")
+	for _, line := range lines {
+		fields := strings.Fields(line)
+		if fields[0] == "cpu" {
+			numFields := len(fields)
+			for i := 1; i < numFields; i++ {
+				val, err := strconv.ParseUint(fields[i], 10, 64)
+				if err != nil {
+					fmt.Println("Error: ", i, fields[i], err)
+				}
+				total += val // tally up all the numbers to get total ticks
+				if i == 4 {  // idle is the 5th field in the cpu line
+					idle = val
+				}
+			}
+			return
+		}
+	}
+	return
+}
+
+func getCpuUsageInfo() (usage, busy, total float64) {
+	idle0, total0 := getCPUSample()
+	time.Sleep(3 * time.Second)
+	idle1, total1 := getCPUSample()
+
+	idleTicks := float64(idle1 - idle0)
+	totalTicks := float64(total1 - total0)
+	cpuUsage := 100 * (totalTicks - idleTicks) / totalTicks
+	return cpuUsage, totalTicks - idleTicks, totalTicks
+	//fmt.Printf("CPU usage is %f%% [busy: %f, total: %f]\n", cpuUsage, totalTicks-idleTicks, totalTicks)
+}
 func check(pushdata *puthFields, today *int) {
 	v, _ := mem.VirtualMemory()
-	percent, _ := cpu.Percent(time.Second, false)
+	percent, _, _ := getCpuUsageInfo()
 
 	memStr, cpuStr := "", ""
 	if v.Total-v.Used < config.memUsable<<20 {
 		memStr = fmt.Sprintf("##### 可用内存不足 %dMB\n", config.memUsable)
 	}
-	if percent[0] > config.cpuUseRate {
+	if percent > config.cpuUseRate {
 		cpuStr = fmt.Sprintf("##### CPU使用率超过 %.f%%\n", config.cpuUseRate)
 	}
 	str := memStr + cpuStr
@@ -113,13 +149,13 @@ func check(pushdata *puthFields, today *int) {
 	str = str + "######" + fmt.Sprintf(" 警告代码: %s \n", randstr)
 	writeStr := str
 	if cpuStr != "" {
-		cmd := exec.Command("/bin/bash", "-c", "ps aux|head -1;ps auxw|sort -rn -k3|head -10|tee")
+		cmd := exec.Command("/bin/bash", "-c", "ps aux|head -1;ps auxw|sort -rn -k3|head -10")
 		if output, err := cmd.Output(); err == nil {
 			writeStr = writeStr + "CPU TOP10: \n" + string(output)
 		}
 	}
 	if memStr != "" {
-		cmd := exec.Command("/bin/bash", "-c", "ps aux|head -1;ps auxw|sort -rn -k4|head -10|tee")
+		cmd := exec.Command("/bin/bash", "-c", "ps aux|head -1;ps auxw|sort -rn -k4|head -10")
 		if output, err := cmd.Output(); err == nil {
 			if cpuStr != "" {
 				writeStr = writeStr + "\n"
@@ -144,7 +180,7 @@ func check(pushdata *puthFields, today *int) {
 		"###### 警告发生时间: %s \n"+
 		"###### 服务器名称: %s \n",
 		v.Total>>20, v.Used>>20, v.UsedPercent,
-		(percent[0]),
+		(percent),
 		pushdata.todayRunCount,
 		pushdata.allRunCount,
 		time.Now().Format("2006-01-02 15:04:05"),
